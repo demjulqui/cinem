dddcd67f25e3ef2ee35ac817a57fae3816c561a2
demerson <demjulqui@gmail.com>
2022-05-14 18:08:09 +0200

    Aggiunta di piu pagine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Album from './components/Album';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Pages/Layout';
+import Home from './Pages/Home';
+import Blogs from './Pages/Blogs';
+import Contact from './Pages/Contact';
+import NoPage from './Pages/NoPage';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,8 @@
+import Album from "../components/Album";
+
+
+const Home = () => {
+    return <Album />;
+};
+
+export default Home;
