fceedbd5aa2c3f6c441c36f2586d6a126b278cfe
demerson <demjulqui@gmail.com>
2022-05-16 16:18:35 +0200

    modifiche sull'immagine della ricerca

--- src/components/Models/CardD.jsx
+++ src/components/Models/CardD.jsx
@@ -7,6 +7,7 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Figure } from 'react-bootstrap';
 
 export default function MediaControlCard(props) {
 	const theme = useTheme();
@@ -22,20 +23,20 @@ export default function MediaControlCard(props) {
 									<Typography component="div" variant="h5">
 										{props.title}
 									</Typography>
+
 									<Typography component="div" variant="subtitle1">
 										{props.overview}
 									</Typography>
 								</Col>
-							</Row>
-							<Row>
-								<Typography variant="subtitle1" color="text.secondary" component="div">
-									{props.release_date}
-								</Typography>
+								<Col>
+									<Typography variant="subtitle1" color="text.secondary" component="div">
+										{props.release_date}
+									</Typography>
+								</Col>
 							</Row>
 						</CardContent>
-						<Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }} />
 					</Box>
-					<CardMedia component="img" sx={{ width: 151 }} src={props.poster} alt={props.title} />
+					<Figure.Image width={210} height={100} src={props.poster} alt={props.title} />
 				</Card>
 			</Card>
 		</Container>
